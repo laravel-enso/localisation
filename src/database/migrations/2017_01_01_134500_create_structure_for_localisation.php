2e48e13995be714de14064810ab2873c1bac8ad3
mishu <mishu@xtelecom.ro>
2017-06-23 14:14:45 +0300

    updated structure migration

--- src/database/migrations/2017_01_01_134500_create_structure_for_localisation.php
+++ src/database/migrations/2017_01_01_134500_create_structure_for_localisation.php
@@ -5,25 +5,25 @@ use LaravelEnso\Core\app\Classes\StructureManager\StructureMigration;
 class CreateStructureForLocalisation extends StructureMigration
 {
     protected $permissionGroup = [
-        'name' => 'system.localisation', 'description' => 'Localisation Permissions Group',
+        'name' => 'system.localisation', 'description' => 'Localisation permissions group',
     ];
 
     protected $permissions = [
         ['name' => 'system.localisation.initTable', 'description' => 'Init table data for localisation', 'type' => 0, 'default' => false],
         ['name' => 'system.localisation.getTableData', 'description' => 'Get table data for localisation', 'type' => 0, 'default' => false],
-        ['name' => 'system.localisation.create', 'description' => 'Create Langugage', 'type' => 1, 'default' => false],
-        ['name' => 'system.localisation.edit', 'description' => 'Edit Language', 'type' => 1, 'default' => false],
-        ['name' => 'system.localisation.editTexts', 'description' => 'Edit Language File', 'type' => 1, 'default' => false],
-        ['name' => 'system.localisation.getLangFile', 'description' => 'Get Selected Lang File Content', 'type' => 0, 'default' => false],
-        ['name' => 'system.localisation.index', 'description' => 'Localisation Index', 'type' => 0, 'default' => false],
-        ['name' => 'system.localisation.saveLangFile', 'description' => 'Save Lang File', 'type' => 1, 'default' => false],
-        ['name' => 'system.localisation.store', 'description' => 'Save Language', 'type' => 1, 'default' => false],
+        ['name' => 'system.localisation.create', 'description' => 'Create langugage', 'type' => 1, 'default' => false],
+        ['name' => 'system.localisation.edit', 'description' => 'Edit language', 'type' => 1, 'default' => false],
+        ['name' => 'system.localisation.editTexts', 'description' => 'Edit lang file', 'type' => 1, 'default' => false],
+        ['name' => 'system.localisation.getLangFile', 'description' => 'Get selected lang file content', 'type' => 0, 'default' => false],
+        ['name' => 'system.localisation.index', 'description' => 'Localisation index', 'type' => 0, 'default' => false],
+        ['name' => 'system.localisation.saveLangFile', 'description' => 'Save edited lang file', 'type' => 1, 'default' => false],
+        ['name' => 'system.localisation.store', 'description' => 'Save newly created language', 'type' => 1, 'default' => false],
         ['name' => 'system.localisation.update', 'description' => 'Save edited language', 'type' => 1, 'default' => false],
-        ['name' => 'system.localisation.destroy', 'description' => 'Delete Language', 'type' => 1, 'default' => false],
+        ['name' => 'system.localisation.destroy', 'description' => 'Delete language', 'type' => 1, 'default' => false],
     ];
 
     protected $menu = [
-        'name' => 'Localisation', 'icon' => 'fa fa-fw fa-language', 'link' => 'system/localisation', 'has_children' => 0,
+        'name' => 'Localisation', 'icon' => 'fa fa-fw fa-language', 'link' => 'system/localisation', 'has_children' => false,
     ];
 
     protected $parentMenu = 'System';
