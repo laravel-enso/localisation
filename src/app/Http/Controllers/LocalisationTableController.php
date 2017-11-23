962d3b0f8b96eea7c2d8cb910eef7c94b0f6a8b3
Adrian Ocneanu <aocneanu@gmail.com>
2017-11-23 18:34:24 +0000

    Apply fixes from StyleCI

--- src/app/Http/Controllers/LocalisationTableController.php
+++ src/app/Http/Controllers/LocalisationTableController.php
@@ -11,7 +11,7 @@ class LocalisationTableController extends Controller
 {
     use Datatable, Excel;
 
-    private const Template = __DIR__ . '/../../Tables/localisation.json';
+    private const Template = __DIR__.'/../../Tables/localisation.json';
 
     public function query()
     {
