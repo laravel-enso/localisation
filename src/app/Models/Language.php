e60697beaf2554c8fbf8b775afd93d238088ac94
mishu <mishu@xtelecom.ro>
2017-12-20 08:40:13 +0200

    adds .styleci.yml; updates composer.json for enso/helpers

--- src/app/Models/Language.php
+++ src/app/Models/Language.php
@@ -3,12 +3,9 @@
 namespace LaravelEnso\Localisation\app\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use LaravelEnso\Helpers\Traits\FormattedTimestamps;
 
 class Language extends Model
 {
-    use FormattedTimestamps;
-
     protected $fillable = ['name', 'display_name', 'flag'];
 
     public static function scopeExtra($query)
