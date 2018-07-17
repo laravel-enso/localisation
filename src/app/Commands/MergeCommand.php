3b7f5b3542533aaf943536a00d2fefb8e8db0c07
mishu <mishu@xtelecom.ro>
2018-07-17 10:24:30 +0300

    fixes merge command for testing env

--- src/app/Commands/MergeCommand.php
+++ src/app/Commands/MergeCommand.php
@@ -3,6 +3,7 @@
 namespace LaravelEnso\Localisation\app\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Schema;
 use LaravelEnso\Localisation\app\Classes\Json\Merger;
 
 class MergeCommand extends Command
@@ -14,6 +15,11 @@ class MergeCommand extends Command
     {
         $locale = $this->option('locale');
 
+        if (!Schema::hasTable('languages')) {
+            $this->info('Couldn\'t merge the localisation files because the migrations were not ran.');
+
+            return;
+        }
         (new Merger())->run($locale);
 
         $this->info('Language files merged ('.($locale ?: 'all').')!');
