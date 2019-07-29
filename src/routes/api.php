8e9090c9d1eef9a429bba8a7cf680c9000e33f8e
Bohdan Stavr <admirj24@gmail.com>
2019-07-29 15:06:57 +0300

    closes #59

--- src/routes/api.php
+++ src/routes/api.php
@@ -5,6 +5,15 @@ Route::middleware(['web', 'auth', 'core'])
     ->prefix('api/system/localisation')
     ->as('system.localisation.')
     ->group(function () {
+        Route::namespace('Json')
+            ->group(function () {
+                Route::get('editTexts', 'Index')->name('editTexts');
+                Route::get('getLangFile/{language}/{subDir}', 'Edit')->name('getLangFile');
+                Route::patch('saveLangFile/{language}/{subDir}', 'Update')->name('saveLangFile');
+                Route::patch('addKey', 'AddKey')->name('addKey');
+                Route::patch('merge/{locale?}', 'Merge')->name('merge');
+            });
+
         Route::namespace('Language')
             ->group(function () {
                 Route::get('create', 'Create')->name('create');
@@ -16,14 +25,5 @@ Route::middleware(['web', 'auth', 'core'])
                 Route::get('initTable', 'InitTable')->name('initTable');
                 Route::get('tableData', 'TableData')->name('tableData');
                 Route::get('exportExcel', 'ExportExcel')->name('exportExcel');
-            });
-
-        Route::namespace('Json')
-            ->group(function () {
-                Route::get('editTexts', 'Index')->name('editTexts');
-                Route::get('getLangFile/{language}/{subDir}', 'Edit')->name('getLangFile');
-                Route::patch('saveLangFile/{language}/{subDir}', 'Update')->name('saveLangFile');
-                Route::patch('addKey', 'AddKey')->name('addKey');
-                Route::patch('merge/{locale?}', 'Merge')->name('merge');
             });
     });
